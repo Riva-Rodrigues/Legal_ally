57e34eb639fa207a2f5070ca474d0f47d8e17c3b
shktabish <shktab1sh93@gmail.com>
2025-01-29 13:01:26 +0530

    initialize client and server structure with basic configurations and components

---
+++ client/src/App.jsx
@@ -0,0 +1,25 @@
+import { Route, Routes } from "react-router-dom";
+import AppLayout from "./components/AppLayout";
+import SignInPage from "./pages/SignIn";
+import SignUpPage from "./pages/SignUp";
+import LandingPage from "./pages/LandingPage";
+import OnboardingPage from "./pages/OnboardingPage";
+
+const App = () => {
+    return (
+        <Routes>
+            {/* Public Routes */}
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/sign-in" element={<SignInPage />} />
+            <Route path="/sign-up" element={<SignUpPage />} />
+            <Route path="/onboarding" element={<OnboardingPage />} />
+
+            {/* Private Nested Routes */}
+            <Route path="/" element={<AppLayout />}>
+                <Route path="/dashboard" element={<div className="w-full">Dashboard</div>} />
+            </Route>
+        </Routes>
+    );
+};
+
+export default App;
